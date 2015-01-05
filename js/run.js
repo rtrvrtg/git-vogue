ee77b7d6eaddd7bf206aa4a499f5f26533beffaa
Geoffrey Roberts <groberts@anchor.net.au>
2015-01-05 13:19:59 +1100

    Updated styles, images and docs

--- js/run.js
+++ js/run.js
@@ -1,12 +1,25 @@
 $(document).ready( function() {
+	var activate = function(hash){
+		$('#navigation a.active').removeClass('active');
+        $('#navigation a[href="' + window.location.hash + '"]').addClass('active');
+
+        $('#content .active').removeClass('active');
+        $('#content ' + hash).addClass('active');
+	};
+
     smoothScroll.init({
     	offset: 10,
     	easing: "easeInOutCubic",
     	callbackBefore: function (toggle, anchor) {
-    		$('#navigation a.active').removeClass('active');
-    		$(toggle).addClass('active');
+    		activate(window.location.hash);
     	}
     });
+    if (window.location.hash == "" || window.location.hash == "#") {
+        activate('#title');
+    }
+    else {
+        activate(window.location.hash);
+    }
     $('em:contains("git-vogue")').addClass('git-vogue-branding');
     $('em:contains("Anchor")').addClass('anchor-branding');
 });
